64a2c32240c27601bb01d80834332782aab9abd2
Xon <tazipper1@hotmail.com>
2015-03-04 21:43:26 +0800

    Reformat whitespace

--- Addon.php
+++ Addon.php
@@ -18,49 +18,49 @@
 abstract class LiamW_AlterEgoDetector_Addon
 {
 
-	public static function install($installedAddon)
-	{
-		if (XenForo_Application::$versionId < 1030070)
-		{
-			throw new XenForo_Exception("Please upgrade XenForo. 1.3+ is required.", true);
-		}
+    public static function install($installedAddon)
+    {
+        if (XenForo_Application::$versionId < 1030070)
+        {
+            throw new XenForo_Exception("Please upgrade XenForo. 1.3+ is required.", true);
+        }
 
-		$versionId = is_array($installedAddon) ? $installedAddon['version_id'] : 0;
+        $versionId = is_array($installedAddon) ? $installedAddon['version_id'] : 0;
 
-		$contentTypeInstaller = LiamW_Shared_DatabaseSchema_Abstract2::create('LiamW_AlterEgoDetector_DatabaseSchema_ContentType');
-		$contentTypeInstaller->install($versionId);
-		$contentTypeFieldInstaller = LiamW_Shared_DatabaseSchema_Abstract2::create('LiamW_AlterEgoDetector_DatabaseSchema_ContentTypeField');
-		$contentTypeFieldInstaller->install($versionId);
+        $contentTypeInstaller = LiamW_Shared_DatabaseSchema_Abstract2::create('LiamW_AlterEgoDetector_DatabaseSchema_ContentType');
+        $contentTypeInstaller->install($versionId);
+        $contentTypeFieldInstaller = LiamW_Shared_DatabaseSchema_Abstract2::create('LiamW_AlterEgoDetector_DatabaseSchema_ContentTypeField');
+        $contentTypeFieldInstaller->install($versionId);
 
-		XenForo_Model::create('XenForo_Model_ContentType')->rebuildContentTypeCache();
-	}
+        XenForo_Model::create('XenForo_Model_ContentType')->rebuildContentTypeCache();
+    }
 
-	public static function uninstall()
-	{
-		$contentTypeInstaller = LiamW_Shared_DatabaseSchema_Abstract2::create('LiamW_AlterEgoDetector_DatabaseSchema_ContentType');
-		$contentTypeInstaller->uninstall();
-		$contentTypeFieldInstaller = LiamW_Shared_DatabaseSchema_Abstract2::create('LiamW_AlterEgoDetector_DatabaseSchema_ContentTypeField');
-		$contentTypeFieldInstaller->uninstall();
+    public static function uninstall()
+    {
+        $contentTypeInstaller = LiamW_Shared_DatabaseSchema_Abstract2::create('LiamW_AlterEgoDetector_DatabaseSchema_ContentType');
+        $contentTypeInstaller->uninstall();
+        $contentTypeFieldInstaller = LiamW_Shared_DatabaseSchema_Abstract2::create('LiamW_AlterEgoDetector_DatabaseSchema_ContentTypeField');
+        $contentTypeFieldInstaller->uninstall();
 
-		// update cache
-		XenForo_Model::create('XenForo_Model_ContentType')->rebuildContentTypeCache();
-	}
+        // update cache
+        XenForo_Model::create('XenForo_Model_ContentType')->rebuildContentTypeCache();
+    }
 
-	public static function extendClass($class, array &$extend)
-	{
-		switch ($class)
-		{
-			case 'XenForo_ControllerPublic_Login':
-				$extend[] = 'LiamW_AlterEgoDetector_Extend_ControllerPublic_Login';
-				break;
-			case 'XenForo_Model_SpamPrevention':
-				$extend[] = 'LiamW_AlterEgoDetector_Extend_Model_SpamPrevention';
-				break;
-		}
-	}
-	
-	// This is a stub to allow the upgrade to go smoothly
-	public static function initDependencies(XenForo_Dependencies_Abstract $dependencies, array $data)
-	{
-	}
+    public static function extendClass($class, array &$extend)
+    {
+        switch ($class)
+        {
+            case 'XenForo_ControllerPublic_Login':
+                $extend[] = 'LiamW_AlterEgoDetector_Extend_ControllerPublic_Login';
+                break;
+            case 'XenForo_Model_SpamPrevention':
+                $extend[] = 'LiamW_AlterEgoDetector_Extend_Model_SpamPrevention';
+                break;
+        }
+    }
+
+    // This is a stub to allow the upgrade to go smoothly
+    public static function initDependencies(XenForo_Dependencies_Abstract $dependencies, array $data)
+    {
+    }
 }
