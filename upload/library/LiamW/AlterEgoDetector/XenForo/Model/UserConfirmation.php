babbb6ed657073e90f7ed95377056cfa14238aa9
Xon <tazipper1@hotmail.com>
2016-08-28 09:19:02 +0800

    Option to require moderated users todo email confirmation

---
+++ upload/library/LiamW/AlterEgoDetector/XenForo/Model/UserConfirmation.php
@@ -0,0 +1,26 @@
+<?php
+
+class LiamW_AlterEgoDetector_XenForo_Model_UserConfirmation extends XFCP_LiamW_AlterEgoDetector_XenForo_Model_UserConfirmation
+{
+    public function processUserModeration(array $user, $action, $notify = true, $rejectionReason = '')
+    {
+        if ($user['user_state'] != 'moderated')
+        {
+            return false;
+        }
+
+        if ($action == 'aed_email_confirm')
+        {
+            $dw = XenForo_DataWriter::create('XenForo_DataWriter_User');
+            $dw->setExistingData($user);
+            $dw->advanceRegistrationUserState();
+            $dw->save();
+
+            $this->sendEmailConfirmation($dw->getMergedData());
+
+            return true;
+        }
+
+        return parent::processUserModeration($user, $action, $notify, $rejectionReason);
+    }
+}
